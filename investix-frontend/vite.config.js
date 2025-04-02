5b75d68104a8ddf075e57fb39732050dd25408e1
matheusgiombelli <87329314+matheusgiombelli@users.noreply.github.com>
2025-04-01 21:22:39 -0300

    arquivos necessários
    adicionei alguns arquivos importantes para o funcionamento correto de algumas bibliotecas

--- investix-frontend/vite.config.js
+++ investix-frontend/vite.config.js
@@ -1,19 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import path from 'node:path'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
-// import vueDevTools from 'vite-plugin-vue-devtools'
-// import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
     plugins: [
-        vue(),
-        // vueDevTools(),
-        // tailwindcss()
-    ],
+        vue(), 
+        tailwindcss()],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+        '@': path.resolve(__dirname, './src'),
         },
     },
 })
