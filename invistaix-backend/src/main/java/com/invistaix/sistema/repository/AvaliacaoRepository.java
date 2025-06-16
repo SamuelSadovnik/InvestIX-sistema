7c9f723f92e43fad3edcacb1dbfa4e12d6abf85a
Matheus Emanuel Giombelli <87329314+matheusgiombelli@users.noreply.github.com>
2025-06-16 20:30:36 -0300

    Delete invistaix-backend directory

--- invistaix-backend/src/main/java/com/invistaix/sistema/repository/AvaliacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.invistaix.sistema.repository;
-
-import com.invistaix.sistema.model.Avaliacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Integer> {
-}
--- invistaix-backend/src/main/java/com/invistaix/sistema/repository/ImpostoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.invistaix.sistema.repository;
-
-import com.invistaix.sistema.model.Imposto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImpostoRepository extends JpaRepository<Imposto, Integer> {
-}
